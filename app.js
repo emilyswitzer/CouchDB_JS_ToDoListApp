cf9bab9c15236d0b506c41e7bb8cf8eb51304ff4
Emily Switzer <emily-switzer@live.com>
2021-10-16 14:38:46 +0100

    Updated CRUD with nano

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const path = require('path');
 const NodeCouchDb = require('node-couchdb');
-const nano = require('nano')('http://localhost:5984');
+const nano = require('nano')('http://emily:emily12345@localhost:5984');
 
-const couch = new NodeCouchDb({
-    auth: {
-        user:'emily',
-        password:'emily12345'
-    }
-});
+// const couch = new NodeCouchDb({
+//     auth: {
+//         user:'emily',
+//         password:'emily12345'
+//     }
+// });
 
 const todolist = nano.db.use('todolist');
 
 
 
 
-const dbName = 'todolist';
+// const dbName = 'todolist';
 const viewUrl = '/_design/all_tasks/_view/all';
 
-couch.listDatabases().then(function(dbs){
-   console.log(dbs);
-});
+// couch.listDatabases().then(function(dbs){
+//    console.log(dbs);
+// });
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -31,40 +31,102 @@ app.use("/static", express.static("public"));
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
-app.get('/', function(req, res){
-   couch.get(dbName, viewUrl).then(
-        function(data, headers, status){
-           console.log(data.data.rows);
-            res.render('index',{
-                tasks:data.data.rows
-            });
-
-        },
-        function(err){
-            res.send(err);
+let total=0;
+const completeTasks = 
+    todolist.view("complete_tasks", "all",
+    function(err, data){
+        if(!err){
+            console.log(data);
+            total = data.rows[0].value;
+            console.log(total);
+        }else{
+            res.render(err);
         }
 
-    );
+    });
 
-   });
+
+app.get('/', function(req, res){
+    todolist.view("all_tasks", "all",
+    function(err, data){
+        if(!err){
+            const complete = completeTasks;
+            const tasks=data.rows;
+            res.render('index', {
+                tasks,
+                complete,
+                total
+            });
+        }else{
+            res.render(err);
+        }
+
+    });
+});
+
+
+
+
+
+// app.get('/', function(req, res){
+//    couch.get(dbName, viewUrl).then(
+//         function(data, headers, status){
+//            console.log(data.data.rows);
+//             res.render('index',{
+//                 tasks:data.data.rows
+//             });
+
+//         },
+//         function(err){
+//             res.send(err);
+//         }
+
+//     );
+
+//    });
 
    
 
- app.post('/',(req, res) => {
+//  app.post('/',(req, res) => {
+//     console.log(req.body);
+//     const content = req.body.content;
+    
+//     couch.uniqid().then(function(ids){
+//         const id = ids[0];
+
+   
+//          couch.insert('todolist', {
+//             _id: id,
+//             task: content,
+//             complete: false
+//         }).then(
+//             function(data, headers, status){
+//              res.redirect('/');
+//             },
+//             function(err){
+//                 res.send(err);
+//             }
+//         ) 
+//     })
+
+// }); 
+
+app.post('/',(req, res) => {
     console.log(req.body);
     const content = req.body.content;
     
-    couch.uniqid().then(function(ids){
+    nano.uuids().then(function(ids){
         const id = ids[0];
-
    
-         couch.insert('todolist', {
+         todolist.insert({
             _id: id,
             task: content,
             complete: false
         }).then(
             function(data, headers, status){
+              
              res.redirect('/');
+
             },
             function(err){
                 res.send(err);
@@ -72,30 +134,53 @@ app.get('/', function(req, res){
         ) 
     })
 
-}); 
+});
+
+// app.post('/delete/:id', function(req, res){
+//      const id = req.params.id;
+//      const rev = req.body.rev;
+
+//      couch.del('todolist', id, rev).then(
+//          function(data, headers, status){
+//              res.redirect('/');
+
+//      },
+//      function(err){
+//          res.send(err);
+//      }
+//      )
+// });
 
 app.post('/delete/:id', function(req, res){
-     const id = req.params.id;
-     const rev = req.body.rev;
+    const id = req.params.id;
+    const rev = req.body.rev;
 
-     couch.del('todolist', id, rev).then(
-         function(data, headers, status){
-             res.redirect('/');
+    todolist.destroy(id, rev).then(
+        function(data, headers, status){
+            res.redirect('/');
 
-     },
-     function(err){
-         res.send(err);
-     }
-     )
+    },
+    function(err){
+        res.send(err);
+    }
+    )
+});
+
+app.post('/results', function(req, res){
+
+   if (req.body.celcius) {
+        console.log("true");
+ } else {
+        console.log("false");
+ }
 });
 
 app.post('/update/:id', function(req, res){
     const id = req.params.id;
     const rev = req.body.rev;
     const task = req.body.task;
-    const complete2 = "true";
 
-    couch.update('todolist', {
+    todolist.insert({
         _id: id,
         _rev: rev,
         task: task,
@@ -113,7 +198,7 @@ app.post('/update/:id', function(req, res){
 });
 app.get("/", (req, res) => {
     res.render("index", {
-      x: clicker
+      x: click
     });
   });
 
@@ -121,4 +206,3 @@ app.listen(3000, function (){
     console.log('Server Started on Port 3000');
 }
 );
-
---
+++ public/script.js
@@ -0,0 +1,7 @@
+$('#form-check-input').change(function() {
+    if ($(this).prop('checked')) {
+      $.post(ajax_url, function(data) {
+      
+      })
+    }
+  })
